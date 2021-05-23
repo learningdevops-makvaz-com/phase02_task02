b851bfd3397df6917f7868a2b3e1b7a62c80dc77
Dan <dan@Dans-MacBook-Pro.local>
2021-05-23 08:44:04 -0500

    wp-config.php -> Added support for env variables

--- wp-config.php
+++ wp-config.php
@@ -1,16 +1,16 @@
 <?php
 
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username */
-define( 'DB_USER', 'wp_user' );
+define( 'DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wp_password' );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL hostname */
-define( 'DB_HOST', 'database' );
+define( 'DB_HOST', getenv('DB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
