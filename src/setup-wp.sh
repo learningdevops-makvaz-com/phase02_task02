3e410c6ae5c3a35b42cebb1f70491eded18bafa7
devanbenz <devandbenz@gmail.com>
2021-06-11 17:26:02 -0500

    update theme

--- src/setup-wp.sh
+++ src/setup-wp.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 sleep 30
-wp core install --url=localhost:9000 --title=DockerWP --admin_user=admin --admin_password=$WP_PW --admin_email=admin@mail.com --path=/var/www/wordpress --allow-root
-sleep 5
+wp core install --url=localhost:9000 --title=DockerWP --admin_user=admin --admin_password=$WP_PW --admin_email=admin@mail.com --path=$WP_PATH --allow-root
+
+sleep 1
 if [ $TAG -eq 1 ]
 then
-    wp plugin install /plugins/$WP_PLUGIN --activate --allow-root --path=/var/www/wordpress
+    wp plugin install /plugins/$WP_PLUGIN --activate --allow-root --path=$WP_PATH
+elif [ $TAG -eq 2 ]
+then
+    wp plugin install /plugins/$WP_PLUGIN2 --activate --allow-root --path=$WP_PATH    
 fi
 
-if [ $TAG -eq 2 ]
-then
-    wp plugin install /plugins/$WP_PLUGIN2 --activate --allow-root --path=/var/www/wordpress    
-fi
+sleep 1
+wp theme install $WP_THEME --activate --path=$WP_PATH --allow-root
