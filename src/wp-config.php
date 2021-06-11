4f93d90eb7de3d2fb4af357c9752d9f4e9ad6e13
devanbenz <devandbenz@gmail.com>
2021-06-11 15:57:50 -0500

    update env vars

--- src/wp-config.php
+++ src/wp-config.php
@@ -18,9 +18,10 @@
  * @package WordPress
  */
 
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', getenv('DB_NAME') );
 
 /** MySQL database username */
 define( 'DB_USER', 'wpuser' );
