d55ca3bf4b0d03ba561133c1351a70e62bae6352
devanbenz <devandbenz@gmail.com>
2021-06-10 19:28:22 -0500

    Adding environment variables so I don't need to hardcode stuff

--- src/setup-wp.sh
+++ src/setup-wp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 30
-wp core install --url=localhost:9000 --title=DockerWP --admin_user=admin --admin_password=pass123 --admin_email=admin@mail.com --path=/var/www/wordpress --allow-root
+wp core install --url=localhost:9000 --title=DockerWP --admin_user=admin --admin_password=$WP_PW --admin_email=admin@mail.com --path=/var/www/wordpress --allow-root
 sleep 5
-wp plugin install /plugins/thank-after-post-0.9.0.zip --activate --allow-root --path=/var/www/wordpress
+wp plugin install /plugins/$WP_PLUGIN --activate --allow-root --path=/var/www/wordpress
