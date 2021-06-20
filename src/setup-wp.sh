0b07a6215b6ddef16b89e3785708cbcafa75faec
devanbenz <devandbenz@gmail.com>
2021-06-20 07:12:29 -0500

    Added small modifications to env variables

--- src/setup-wp.sh
+++ src/setup-wp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 30
 # needed to hardcode title - env vars and spaces don't mix well
-wp core install --url=$URL --title="$WP_TITLE" --admin_user=$WP_ADM --admin_password=$WP_PW --admin_email=$ADM_EMAIL --path=$WP_PATH --allow-root
+wp core install --url=$URL --title="${WP_TITLE}" --admin_user=$WP_ADM --admin_password=$WP_PW --admin_email=$ADM_EMAIL --path=$WP_PATH --allow-root
 
 sleep 1
 wp plugin install $WP_PLUGIN --activate --allow-root --path=$WP_PATH
